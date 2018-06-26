0594308f6a9515340ebd625d144717d9b19b2980
Frederick Glazier <frederick.glazier@desjardins.com>
2018-06-26 12:50:02 -0400

    test

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,21 +1,30 @@
 import React, {Component} from 'react'
 import QuizQuestion from "./QuizQuestion";
+import QuizEnd from "./QuizEnd";
 
 
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
 
+
     constructor(props) {
         super(props);
         this.state = {quiz_position: 1};
     }
 
-    render() {
-        return (<div>
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
 
-        </div>);
+    render() {
+
+        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+
+        if (!isQuizEnd) {
+            return (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>);
+        } else {
+            return (<QuizEnd></QuizEnd>);
+        }
+
+
     }
 }
 
