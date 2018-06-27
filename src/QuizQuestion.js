b842374a7582b13c124a58d5a2e9f9604c1919f9
Glaiz <frederick.glazier@desjardins.com>
2018-06-27 07:47:06 -0400

    tt

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,8 +1,13 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
 
+
+    constructor(props) {
+        super(props);
+
+    }
 
     render() {
         return (
@@ -18,7 +23,7 @@ class QuizQuestion extends Component {
                         {this.props.quiz_question.answer_options.map((answer_option, index) => {
 
                             return <QuizQuestionButton key={index} button_text={answer_option}
-                                                       clickHandler={this.handleClick.bind(this)}/>
+                                clickHandler={this.handleClick.bind(this)} />
                         })}
 
                     </ul>
