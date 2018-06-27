0f19a51b59301128f924c00f72f31277816772f8
Glaiz <frederick.glazier@desjardins.com>
2018-06-27 08:44:18 -0400

    ytuyu

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -6,12 +6,14 @@ class QuizQuestion extends Component {
 
     constructor(props) {
         super(props);
+        this.state = { incorrectAnswer: false };
 
     }
 
     render() {
         return (
             <main>
+
                 <section>
                     <p>
                         {this.props.quiz_question.instruction_text}
@@ -28,13 +30,19 @@ class QuizQuestion extends Component {
 
                     </ul>
                 </section>
+                {this.state.incorrectAnswer?  <p className='error' > Sorry,that's not right  </p> : null }
             </main>
         )
     }
 
     handleClick(buttonText) {
-        if (buttonText === this.props.quiz_question.answer)
+        if (buttonText === this.props.quiz_question.answer) {
+            this.setState({ incorrectAnswer: false })
             this.props.showNextQuestionHandler()
+        } else {
+            this.setState({ incorrectAnswer: true })
+        }
+
     }
 
 }
